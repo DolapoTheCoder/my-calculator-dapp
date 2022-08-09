df401468dbc87c6e6b18dd8c681963ab5507fd4d
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-08-09 14:46:50 +0100

    create 1st Sol Func

--- programs/my-calculator-dapp/src/lib.rs
+++ programs/my-calculator-dapp/src/lib.rs
@@ -5,11 +5,11 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod my_calculator_dapp {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //solana is stateless
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
         Ok(())
-    }
+    } 
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
